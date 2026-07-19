b2317ba615691401b3f14105e8c893de47f7d651
666forever <notcbandi@gmail.com>
2026-07-19 03:27:35 +0200

    Fix Pages Functions manifest import

--- server/assets.js
+++ server/assets.js
@@ -1,4 +1,4 @@
-import assets from '../src/generated/assets.json' with { type: 'json' };
+import assets from '../src/generated/assets.json';
 
 const assetMap = new Map(assets.map(asset => [asset.id, asset]));
 export const getTrustedAsset = id => assetMap.get(id) || null;
